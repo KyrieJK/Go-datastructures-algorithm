1c7a4045ae37490f14b37a785d8895e2c15044ba
KyrieJK <18910599692@163.com>
2019-07-16 23:43:41 +0800

    二叉树数据结构、遍历算法补充

--- BinaryTree/BinaryTree.go
+++ BinaryTree/BinaryTree.go
@@ -34,3 +34,49 @@ func (tree *BinaryTree) InsertItem(i int) {
 		}
 	}
 }
+
+func (tree *BinaryTree) SearchItem(i int) (*Node,bool){
+	if tree.root == nil{
+		return nil,false
+	}
+
+	currentNode := tree.root
+
+	for currentNode != nil{
+		if i == currentNode.data{
+			return currentNode.data
+		}else if i>currentNode.data{
+			currentNode = currentNode.right
+		}else if i<currentNode.data{
+			currentNode = currentNode.left
+		}
+	}
+	return nil,false
+}
+
+/* 二叉树前序遍历 */
+func (tree *BinaryTree) PreorderTraversal(rootNode *Node,callback func(int)){
+	if root != nil{
+		callback(rootNode.data)
+		PreorderTraversal(rootNode.left,callback)
+		PreorderTraversal(rootNode.right,callback)
+	}
+}
+
+/* 二叉树中序遍历 */
+func (tree *BinaryTree) InorderTraversal(rootNode *Node,callback func(int)){
+	if rootNode!=nil{
+		InorderTraversal(rootNode.left,callback)
+		callback(rootNode.data)
+		InorderTraversal(rootNode.right,callback)
+	}
+}
+
+/* 二叉树后续遍历 */
+func (tree *BinaryTree) PostTraversal(rootNode *Node,callback func(int)){
+	if rootNode!=nil{
+		PostTraversal(rootNode.left,callback)
+		PostTraversal(rootNode.right,callback)
+		callback(rootNode.data)
+	}
+}
