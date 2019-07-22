beb2ad1b60b0a8d350397cc03463e41068dd7e58
KyrieJK <18910599692@163.com>
2019-07-22 13:39:16 +0800

    归并排序算法实现

---
+++ MergeSort/MergeSort.go
@@ -0,0 +1,44 @@
+package MergeSort
+
+func MergeSort(array []int,leftIndex,rightIndex int){
+	if leftIndex < rightIndex{
+		midIndex := int((leftIndex+rightIndex)/2)
+		MergeSort(array,leftIndex,midIndex)
+		MergeSort(array,midIndex+1,rightIndex)
+		
+	}
+}
+
+func merge(array []int,leftIndex,midIndex,rightIndex int){
+	var tempArray1,tempArray2 []int
+	for i:=leftIndex;i<=midIndex;i++{
+		tempArray1 = append(tempArray1,array[i])
+	}
+	for i:=midIndex+1;i<=rightIndex;i++{
+		tempArray2 = append(tempArray2,array[i])
+	}
+
+	arrayIndex := leftIndex
+	tempArray1Index:=0
+	tempArray2Index:=0
+	for tempArray1Index<len(tempArray1) && tempArray2Index<len(tempArray2){
+		if tempArray1[tempArray1Index] <= tempArray2[tempArray2Index]{
+			array[arrayIndex] = tempArray1[tempArray1Index]
+			tempArray1Index++
+		}else {
+			array[arrayIndex] = tempArray2[tempArray2Index]
+			tempArray2Index++
+		}
+		arrayIndex++
+	}
+	for tempArray1Index < len(tempArray1){
+		array[arrayIndex] = tempArray1[tempArray1Index]
+		tempArray1Index++
+		arrayIndex++
+	}
+	for tempArray2Index<len(tempArray2){
+		array[arrayIndex] = tempArray2[tempArray2Index]
+		tempArray2Index++
+		arrayIndex++
+	}
+}
